5952fe59dedda381adbca3629b3fef1958c6ff93
gavrilovds <dimagavrilov2004@yandex.ru>
2023-11-27 19:14:15 +0300

    hw8: FibonacciWithCustomThreadPoolTest fix

--- src/test/java/edu/hw8/task2/FibonacciWithCustomThreadPoolTest.java
+++ src/test/java/edu/hw8/task2/FibonacciWithCustomThreadPoolTest.java
@@ -2,6 +2,8 @@ package edu.hw8.task2;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.CountDownLatch;
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -15,7 +17,7 @@ public class FibonacciWithCustomThreadPoolTest {
     public void getFib_shouldCalculateFibForDifferentNumbersInMultipleThreads() {
         ThreadPool threadPool = FixedThreadPool.create(4);
         List<Integer> expected = List.of(0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55);
-        final List<Integer> actual = new ArrayList<>();
+        final List<Integer> actual = new CopyOnWriteArrayList<>();
         for (int i = 0; i <= 10; i++) {
             final int cur = i;
             threadPool.execute(() -> {
